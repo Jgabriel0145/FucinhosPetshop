878c8b9f064051d68bd5519897469d77551b134d
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-08-23 23:22:08 -0300

    Correções no banco e cadastro de animais.

--- App/Model/AnimalModel.php
+++ App/Model/AnimalModel.php
@@ -2,7 +2,31 @@
 
 namespace App\Model;
 
+use App\DAO\AnimalDAO;
+
 class AnimalModel extends Model
 {
-    
+    public $id, $nome, $raca, $peso, $porte, $id_cliente;
+
+    public function Save()
+    {
+        if ($this->id == null) (new AnimalDAO())->Insert($this);
+        
+        else (new AnimalDAO())->Update($this);
+    }
+
+    public function GetAllRows()
+    {
+        return $this->rows = (new AnimalDAO())->Select();
+    }
+
+    public function SearchById($id)
+    {
+        return (new AnimalDAO())->SearchById($id);
+    }
+
+    public function Delete($id)
+    {
+        (new AnimalDAO())->Delete($id);
+    }
 }
