5dc310fe61a3ddb6456d8df6a728e9f9067f0c46
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-09-03 20:23:34 -0300

    Servico.

--- App/Controller/ServicoController.php
+++ App/Controller/ServicoController.php
@@ -2,25 +2,42 @@
 
 namespace App\Controller;
 
+use App\Model\ServicoModel;
+
 class ServicoController extends Controller
 {
     static public function Form()
     {
+        parent::IsAuthenticated();
+        
         parent::render('Servico/ServicoCadastro');
     }
 
     static public function Save() 
     {
-        
+        parent::IsAuthenticated();
+
+        $model = new ServicoModel();
+
+        $model->id = $_POST['id'];
+        $model->descricao = $_POST['descricao_servico'];
+        $model->data_servico = $_POST['data_servico'];
+        $model->id_cliente = $_POST['id_cliente_servico'];
+
+        $model->Save();
+
+        header('Location: /servico/listagem');
     }
 
     static public function List()
     {
+        parent::IsAuthenticated();
+
         parent::render('Servico/ServicoListagem');
     }
 
     static public function Delete()
     {
-
+        parent::IsAuthenticated();
     }
 }
--- App/DAO/ServicoDAO.php
+++ App/DAO/ServicoDAO.php
@@ -2,9 +2,38 @@
 
 namespace App\DAO;
 
+use App\Model\ServicoModel;
 use \PDO;
 
 class ServicoDAO extends DAO
 {
-    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function Insert(ServicoModel $model)
+    {
+
+    }
+
+    public function Update(ServicoModel $model)
+    {
+
+    }
+
+    public function Select()
+    {
+        
+    }
+
+    public function SearchById($id)
+    {
+
+    }
+
+    public function Delete($id)
+    {
+
+    }
 }
