f29cb1d3f8ba0001355641cad4e57edd53869e53
mahteixeira <teixeiramarina13@gmail.com>
2023-09-04 08:18:56 -0300

    separando a nav

---
+++ App/Views/Includes/Navbar/navbar.php
@@ -0,0 +1,42 @@
+ 
+<nav class="menu-lateral">
+    <div class="btn-expandir">
+      <i class="bi bi-list" id="expandir"></i>
+    </div>
+      <ul class="menu-principal">
+        <li class="item-menu" id="inicio">
+          <a href="/">
+            <span class="icon"><i class="bi bi-house"></i></span>
+            <span class="txt-link">Inicio</span>
+          </a>
+        </li>
+        <li class="item-menu" id="cadastros">
+          <a href="#">
+            <span class="icon"><i class="bi bi-person-plus"></i></span>
+            <span class="txt-link">Adicionar</span>
+          </a>
+          <!-- <span class="dropdown"><i class="bi bi-caret-down-fill"></i></span> -->
+        </li>
+        <li class="item-menu">
+          <a href="#">
+            <span class="icon">
+              <i class="bi bi-calendar2-plus"></i>
+            </span>
+            <span class="txt-link">Horários</span>
+          </a>
+          <!-- <span class="dropdown"><i class="bi bi-caret-down-fill"></i></span> -->
+        </li>
+        <li class="item-menu">
+          <a href="#">
+            <span class="icon">
+              <i class="bi bi-box2"></i>
+            </span>
+            <span class="txt-link">Estoque</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav><!-- menu lateral -->
+
+
+    <!-- FIM DA SIDEBAR -->
